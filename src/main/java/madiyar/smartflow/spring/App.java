bebcdeb0001e2774afee9b16864269be9ad476a9
Madiyar <wolfenstrun@gmail.com>
2025-11-02 14:08:19 +0500

    init commit

---
+++ src/main/java/madiyar/smartflow/spring/App.java
@@ -0,0 +1,13 @@
+package madiyar.smartflow.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
