e8df3fc759ef65c46c63e7eb7e930ff87220a363
Madiyar <wolfenstrun@gmail.com>
2025-11-02 14:25:11 +0500

    metrics commit

--- src/main/java/madiyar/smartflow/spring/Metrics.java
+++ src/main/java/madiyar/smartflow/spring/Metrics.java
@@ -1,4 +1,37 @@
 package madiyar.smartflow.spring;
 
 public class Metrics {
+    private long startTime;
+    private long endTime;
+
+    // SCC metrics
+    public int dfsVisits;
+    public int edgesTraversed;
+
+    // Topological sort metrics
+    public int queuePushes;
+    public int queuePops;
+
+    // Shortest path metrics
+    public int relaxations;
+
+    public void startTimer() {
+        startTime = System.nanoTime();
+    }
+
+    public void stopTimer() {
+        endTime = System.nanoTime();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+
+    public void reset() {
+        dfsVisits = 0;
+        edgesTraversed = 0;
+        queuePushes = 0;
+        queuePops = 0;
+        relaxations = 0;
+    }
 }
